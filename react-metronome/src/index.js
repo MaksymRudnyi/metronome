ba6fd99d8b7021b8a04d55727d9d30394a8eb61d
Maksym_Rudnyi <maksym_rudnyi@epam.com>
2018-07-25 14:44:46 +0300

    remove trash

--- react-metronome/src/index.js
+++ react-metronome/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import Metronom from './metronome/Metronome';
 import registerServiceWorker from './registerServiceWorker';
 
