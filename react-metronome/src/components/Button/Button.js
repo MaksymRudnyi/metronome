7f95c6980c87860b6a98f3864624b2b8d5bdf450
Maksym_Rudnyi <maksym_rudnyi@epam.com>
2018-07-26 16:48:32 +0300

    added pritter

--- react-metronome/src/components/Button/Button.js
+++ react-metronome/src/components/Button/Button.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTyps from 'prop-types';
+import styled from 'styled-components';
 
-const Button = ({isPlaying, handleClick}) => {
-	return (
-		<button onClick={handleClick}>
-			{isPlaying ? 'Stop' : 'Start'}
-		</button>
-	);
+const StyledButton = styled.button`
+  background: #c94d46;
+  padding: 10px;
+  border: 1px solid #832420;
+  border-radius: 2px;
+  width: 100px;
+  color: #fff;
+  font-size: 18px;
+
+  &:focus {
+    outline: 0;
+  }
+`;
+
+const Button = ({ isPlaying, handleClick }) => {
+  return (
+    <StyledButton onClick={handleClick}>
+      {isPlaying ? 'Stop' : 'Start'}
+    </StyledButton>
+  );
 };
 
 Button.propTypes = {
-	isPlaying: PropTyps.bool.isRequired,
-	handleClick: PropTyps.func.isRequired
+  isPlaying: PropTyps.bool.isRequired,
+  handleClick: PropTyps.func.isRequired
 };
 
 export default Button;
